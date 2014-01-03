d5b61b622030bdbc5755a2ca0b456f0a794b81b8
Alexsander Akers <a2@pnd.mn>
2014-01-02 19:50:53 -0500

    Replace pdf417-js with bwip-js

--- js/configure.js
+++ js/configure.js
@@ -1,6 +1,85 @@
-PDF417.QUIETV = PDF417.QUIETH = 0;
-PDF417.ROWHEIGHT = 1;
-jQuery(function($) {
+BWIPJS.debug = BWIPJS.load = BWIPJS.print = $.noop;
+$(function() {
+	function Bitmap() {
+		var clr  = 1;
+		var pts  = [];
+		var minx = Infinity;
+		var miny = Infinity;
+		var maxx = 0;
+		var maxy = 0;
+
+		function forEachPoint(callback) {
+			for (var i = 0; i < pts.length; i++) {
+				var pt = pts[i];
+				var x = pt.x - minx;
+				var y = maxy - pt.y; // PostScript builds bottom-up, we build top-down.
+				var color = pt.color;
+				callback(x, y, color);
+			}
+		}
+
+		this.color = function(r, g, b) {
+			clr = +(r + g + b < 3 * 255 / 2);
+		}
+
+		this.pebble_data = function() {
+			var w = maxx - minx + 1;
+			var h = maxy - miny + 1;
+
+			var bytesCount = Math.ceil(w * h / 8);
+
+			var data = [];
+			for (var i = 0; i < bytesCount; i++) data.push(0);
+
+			forEachPoint(function(x, y, color) {
+				var pixelIndex = y * w + x;
+				var byteIndex = Math.floor(pixelIndex / 8);
+				var bitIndex = pixelIndex % 8;
+				var bit = 1 << bitIndex;
+
+				if (color) {
+					data[byteIndex] |= bit;
+				} else {
+					data[byteIndex] &= ~bit;
+				}
+			});
+
+			return [ w, h ].concat(data);
+		}
+
+		this.set = function(x,y) {
+			x = Math.floor(x);
+			y = Math.floor(y);
+			pts.push({
+				x: x,
+				y: y,
+				color: clr
+			});
+			if (minx > x) minx = x;
+			if (miny > y) miny = y;
+			if (maxx < x) maxx = x;
+			if (maxy < y) maxy = y;
+		}
+
+		this.show = function(cvsid) {
+			var cvs = document.getElementById(cvsid);
+
+			var w = 2 * (maxx - minx + 1);
+			var h = 6 * (maxy - miny + 1);
+			cvs.width  = w;
+			cvs.height = h;
+
+			var ctx = cvs.getContext('2d');
+			ctx.fillStyle = '#fff';
+			ctx.fillRect(0, 0, w, h);
+
+			ctx.fillStyle = '#000';
+			forEachPoint(function(x, y, color) {
+				ctx.fillRect(2 * x, 6 * y, 2, 6);
+			});
+		}
+	}
+
 	var payload = {};
 	var cpkEvents = 'change paste keyup';
 
@@ -13,10 +92,11 @@ jQuery(function($) {
 	}
 
 	var validNumberRegEx = /[0-9]{16}/;
+	var $generateBarcode = $('#generate-barcode');
 	var $cardNumber = $('#card-number').on(cpkEvents, function(event) {
 		var text = $(this).val().split(/[^0-9]/).join('');
 		$(this).val(text);
-		$('#generate-barcode').prop('disabled', !validNumberRegEx.test(text));
+		$generateBarcode.prop('disabled', !validNumberRegEx.test(text));
 
 		var left = 16 - text.length;
 		$('#card-number-hint').text(left == 0 ? '' : 'There should be ' + left + ' more digit' + (left == 1 ? '' : 's') + '.');
@@ -24,49 +104,24 @@ jQuery(function($) {
 
 	var cardNumber = getParameterByName('card_number');
 	if (validNumberRegEx.test(cardNumber)) {
-		$cardNumber.val(getParameterByName('card_number')).change();
+		$cardNumber.val(getParameterByName('card_number')).keyup();
 		payload.card_number = cardNumber;
 	}
 
 	$('#barcode-form').submit(function(event) {
 		event.preventDefault();
+		if ($generateBarcode.prop('disabled')) return false;
 
 		var text = $cardNumber.val();
-		PDF417.init(text);
+		var bw = new BWIPJS;
+		var bitmap = new Bitmap;
+		bw.bitmap(bitmap);
+		bw.push(text);
+		bw.push({ rowmult: 1, compact: true });
+		bw.call('pdf417');
+		bitmap.show('barcode');
 
-		var barcode = PDF417.getBarcodeArray();
-
-		var xScale = 2;
-		var yScale = 4;
-
-		var width = barcode.num_cols;
-		var height = barcode.num_rows;
-
-		var canvas = document.getElementById('barcode');
-		canvas.width = xScale * width;
-		canvas.height = yScale * height;
-
-		var ctx = canvas.getContext('2d');
-		var data = [ width, height ], bit = 0, byte = 0;
-
-		for (var row = 0; row < height; row++) {
-			var y = yScale * row;
-			for (var col = 0; col < width; col++) {
-				if (parseInt(barcode.bcode[row][col], 10)) {
-					var x = xScale * col;
-					ctx.fillRect(x, y, xScale, yScale);
-					byte |= 1 << bit;
-				}
-
-				if (++bit == 8) {
-					data.push(byte);
-					bit = byte = 0;
-				}
-			}
-		}
-		if (bit) data.push(byte);
-		
-		payload.barcode_data = data;
+		payload.barcode_data = bitmap.pebble_data();
 		payload.card_number = text;
 		
 		$('#barcode-container').show();
@@ -78,7 +133,7 @@ jQuery(function($) {
 
 	var username = getParameterByName('username');
 	if (username) {
-		$username.val(username).change();
+		$username.val(username).keyup();
 	}
 
 	$('#password').on(cpkEvents, function(event) {
